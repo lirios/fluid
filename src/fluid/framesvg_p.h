b825b1aa385bfc51d21e7ee6707b73a20cd03eb0
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2012-10-28 17:31:13 +0100

    Update notice.

--- src/fluid/framesvg_p.h
+++ src/fluid/framesvg_p.h
@@ -10,18 +10,20 @@
  *    Marco Martin <notmart@gmail.com>
  *    Aaron Seigo <aseigo@kde.org>
  *
- * $BEGIN_LICENSE:LGPL-ONLY$
+ * $BEGIN_LICENSE:LGPL2.1+$
  *
- * This file may be used under the terms of the GNU Lesser General
- * Public License as published by the Free Software Foundation and
- * appearing in the file LICENSE.LGPL included in the packaging of
- * this file, either version 2.1 of the License, or (at your option) any
- * later version.  Please review the following information to ensure the
- * GNU Lesser General Public License version 2.1 requirements
- * will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
  *
- * If you have questions regarding the use of this file, please contact
- * us via http://www.maui-project.org/.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * $END_LICENSE$
  ***************************************************************************/

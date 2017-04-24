2d0ff40bab1b9e480ce9d6a22624605e8c3aabc4
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2017-04-24 23:23:15 +0200

    Update release script

--- scripts/make-release-archive.sh
+++ scripts/make-release-archive.sh
@@ -47,8 +47,7 @@ ${workdir}/fetch_icons.sh || exit $?
 
 {
     git archive --format=tar --prefix=$prefix/ $tag > ${prefix}.tar
-    mkdir -p $prefix/.git
-    cp -R icons $prefix
+    mkdir -p $prefix/.git # for syncqt.pl
     tar --append --file=${prefix}.tar $prefix
     rm -rf $prefix
     xz -9 ${prefix}.tar

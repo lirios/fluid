2a8303ec84861c157349b661f801c71cd8e6ce88
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2017-09-24 16:00:53 +0200

    Add script to regenerate qmltypes

---
+++ scripts/update_qmltypes.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+QMLPLUGINDUMP=${QMLPLUGINDUMP-qmlplugindump}
+
+case $1 in
+-h|--help)
+    echo "Usage: $(basename $0) [IMPORT_PATH]"
+    echo "it uses either '$(which qmlplugindump)' or the one set by 'QMLPLUGINDUMP'"
+    exit 1
+;;
+esac
+
+cmd="${QMLPLUGINDUMP} -noinstantiate -notrelocatable -platform minimal"
+curpath=`dirname $0`
+rootpath=`dirname $(readlink -e $curpath)`
+
+$cmd Fluid.Core 1.0 $1 > $rootpath/src/imports/core/plugins.qmltypes
+$cmd Fluid.Controls 1.0 $1 > $rootpath/src/imports/controls/plugins.qmltypes
+$cmd Fluid.Effects 1.0 $1 > $rootpath/src/imports/effects/plugins.qmltypes
+$cmd Fluid.Layouts 1.0 $1 > $rootpath/src/imports/layouts/plugins.qmltypes

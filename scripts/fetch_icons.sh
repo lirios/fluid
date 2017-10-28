4c38138991fe5342f759d54d26a92d8e7535f2fc
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2017-10-28 18:49:43 +0200

    Add an optional resource file for the icons
    Users that need to reference icons directly have to include the
    resource file in their project.
    
    This is considered an internal detail.
    
    Closes: #154

--- scripts/fetch_icons.sh
+++ scripts/fetch_icons.sh
@@ -3,6 +3,7 @@
 GIT_URL=https://github.com/google/material-design-icons.git
 GIT_DIR=material-design-icons
 TARGET_DIR=icons
+QRC_FILE=$TARGET_DIR/icons.qrc
 
 rm -rf $GIT_DIR
 git clone $GIT_URL
@@ -12,6 +13,8 @@ CATEGORIES=(action av communication device file image maps notification social t
 rm -rf $TARGET_DIR
 mkdir -p $TARGET_DIR
 
+echo "<RCC>
+    <qresource prefix=\"/Fluid/Controls/\">" > $QRC_FILE
 for CATEGORY in ${CATEGORIES[*]}; do
     if [ -d $TARGET_DIR/$CATEGORY ]; then
     	rm -r $TARGET_DIR/$CATEGORY
@@ -26,7 +29,12 @@ for CATEGORY in ${CATEGORIES[*]}; do
         NEW_NAME=$(echo $ICON | sed -E 's/ic_(.*)_48px.svg/\1.svg/')
         cp $FILE $TARGET_DIR/$CATEGORY/$NEW_NAME
         chmod 644 $TARGET_DIR/$CATEGORY/$NEW_NAME
+        echo "        <file>$CATEGORY/$NEW_NAME</file>" >> $QRC_FILE
     done
 done
+
+echo "    </qresource>
+</RCC>
+" >> $QRC_FILE
 
 rm -rf $GIT_DIR

70cb9cd56f13ca335dd3019200ba4e862ae8e42d
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2017-11-13 18:37:48 +0100

    fetch_icons.sh: Fall back to 24px icons
    Some MD icons are provided in the 24px size rather than 48px, fall back
    to 24px if needed.
    
    Issue: #161

--- scripts/fetch_icons.sh
+++ scripts/fetch_icons.sh
@@ -5,6 +5,19 @@ GIT_DIR=material-design-icons
 TARGET_DIR=icons
 QRC_FILE=$TARGET_DIR/icons.qrc
 
+function copy_icon()
+{
+    for FILE in $ICONS; do
+        ICON=$(basename $FILE)
+        NEW_NAME=$(echo $ICON | sed -E 's/ic_(.*)_24px.svg/\1.svg/' | sed -E 's/ic_(.*)_26x24px.svg/\1.svg/' | sed -E 's/ic_(.*)_48px.svg/\1.svg/')
+        if [ ! -f $TARGET_DIR/$CATEGORY/$NEW_NAME ]; then
+            cp $FILE $TARGET_DIR/$CATEGORY/$NEW_NAME
+            chmod 644 $TARGET_DIR/$CATEGORY/$NEW_NAME
+            echo "        <file>$CATEGORY/$NEW_NAME</file>" >> $QRC_FILE
+        fi
+    done
+}
+
 rm -rf $GIT_DIR
 git clone $GIT_URL
 
@@ -23,14 +36,10 @@ for CATEGORY in ${CATEGORIES[*]}; do
     mkdir $TARGET_DIR/$CATEGORY
 
     ICONS=$(ls $GIT_DIR/$CATEGORY/svg/production/*48px*)
+    copy_icon
 
-    for FILE in $ICONS; do
-        ICON=$(basename $FILE)
-        NEW_NAME=$(echo $ICON | sed -E 's/ic_(.*)_48px.svg/\1.svg/')
-        cp $FILE $TARGET_DIR/$CATEGORY/$NEW_NAME
-        chmod 644 $TARGET_DIR/$CATEGORY/$NEW_NAME
-        echo "        <file>$CATEGORY/$NEW_NAME</file>" >> $QRC_FILE
-    done
+    ICONS=$(ls $GIT_DIR/$CATEGORY/svg/production/*24px*)
+    copy_icon
 done
 
 echo "    </qresource>

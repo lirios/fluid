ef0d9507fc9fb70f2f4ec17625b1cbf54bd63269
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2019-06-18 11:22:46 +0200

    Enable clazy with clang CI builds

--- .travis/build.sh
+++ .travis/build.sh
@@ -27,7 +27,10 @@ travis_start "configure"
 msg "Setup CMake..."
 mkdir build
 cd build
-cmake .. \
+if [ "$CXX" == "clang++" ]; then
+    clazy="-DCMAKE_CXX_COMPILER=clazy"
+fi
+cmake .. $clazy \
     -DCMAKE_INSTALL_PREFIX=/usr \
     -DINSTALL_LIBDIR=/usr/lib64 \
     -DINSTALL_QMLDIR=/usr/lib64/qt5/qml \

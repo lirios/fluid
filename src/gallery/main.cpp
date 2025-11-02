cd52baddf95bed5ddd3c9f4f34b0cda3f3aa5205
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2025-11-02 22:59:52 +0100

    Gallery
    feat: gallery application
    
    Initial implementation of the Gallery app.
    
    Closes: #354

---
+++ src/gallery/main.cpp
@@ -0,0 +1,31 @@
+/*
+ * This file is part of Fluid.
+ *
+ * Copyright (C) 2025 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *
+ * $BEGIN_LICENSE:MPL2$
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * $END_LICENSE$
+ */
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+using namespace Qt::StringLiterals;
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+    app.setDesktopFileName("io.liri.Fluid.Gallery"_L1);
+    app.setQuitOnLastWindowClosed(true);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl("qrc:/qml/main.qml"_L1));
+
+    return app.exec();
+}
---
+++ src/gallery/qml/Components.qml
@@ -0,0 +1,20 @@
+/*
+ * This file is part of Fluid.
+ *
+ * Copyright (C) 2025 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *
+ * $BEGIN_LICENSE:MPL2$
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * $END_LICENSE$
+ */
+
+ import QtQuick
+ import Fluid as MD
+
+ MD.Page {
+    title: qsTr("Components")
+ }
